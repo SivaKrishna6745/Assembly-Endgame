3eda8d9bda2aec916ce89da29adec2ebcb7da604
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-04-25 19:44:38 +0530

    - add status component. - UI styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Status from './components/Status';
 
 function App() {
     return (
         <>
             <Header />
+            <Status />
             <main></main>
         </>
     );
