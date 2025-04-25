5ef92b4eaa611ca5eecca8fe562ab8732f3df19b
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-04-25 19:17:05 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1 className="game-title">Assembly: Endgame</h1>
+            <p className="game-desc">
+                Guess the word in under 8 attempts to keep the programming world safe from Assembly!
+            </p>
+        </header>
+    );
+}
+
+export default Header;
