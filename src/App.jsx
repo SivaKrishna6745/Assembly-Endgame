a698dd900737a1b6d07365d309b98e5ce3141b22
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-04-26 21:20:36 +0530

    - Display letters for current word. - Display keyboard buttons and New Game button. - Add guessed letters tracking logic. - Styling changes.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Status from './components/Status';
 import { languages } from './languages.js';
 
 function App() {
+    const [currentWord, setCurrentWord] = useState('react');
+    const [guessedLetters, setGuessedLetters] = useState([]);
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
     const languageChips = languages.map((lang) => {
         const styles = {
             color: lang.color,
@@ -16,10 +21,35 @@ function App() {
         );
     });
 
-    const [word, setWord] = useState('react');
-    const wordArray = Array.from(word);
+    const addGuessedLetter = (letter) => {
+        setGuessedLetters((prevGuessedLetters) =>
+            prevGuessedLetters.includes(letter) ? prevGuessedLetters : [...prevGuessedLetters, letter]
+        );
+    };
 
-    const wordsSection = wordArray.map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+    const guessedLettersSet = new Set(guessedLetters);
+    const currentWordsSection = currentWord.split('').map((letter, index) => (
+        <span key={index} className="flex fl-cntr">
+            {guessedLettersSet.has(letter) && letter.toUpperCase()}
+        </span>
+    ));
+
+    const alphabetArray = Array.from(alphabet);
+    const keyboard = alphabetArray.map((letter) => {
+        const isGuessed = guessedLetters.includes(letter);
+        const isCorrect = isGuessed && currentWord.includes(letter);
+        const isWrong = isGuessed && !currentWord.includes(letter);
+        return (
+            <button
+                type="button"
+                key={letter}
+                className={`btn ${isCorrect && 'success'} ${isWrong && 'failure'}`}
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()}
+            </button>
+        );
+    });
 
     return (
         <>
@@ -27,7 +57,9 @@ function App() {
             <main>
                 <Status />
                 <section className="languages flex fl-cntr">{languageChips}</section>
-                <section className="word flex fl-cntr">{wordsSection}</section>
+                <section className="current-word flex fl-cntr">{currentWordsSection}</section>
+                <section className="keyboard flex fl-cntr">{keyboard}</section>
+                <button className="new-game btn">New Game</button>
             </main>
         </>
     );
