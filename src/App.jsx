f27dcef1a1360234207969a97ece423f3cddc5b9
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-04-26 13:04:08 +0530

    - add language chips from language.js. - styling changes for language chips.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
 import Status from './components/Status';
+import { languages } from './languages.js';
 
 function App() {
+    const languageChips = languages.map((lang) => {
+        const styles = {
+            color: lang.color,
+            backgroundColor: lang.backgroundColor,
+        };
+        return (
+            <span key={lang.name} className="language-chip" style={styles}>
+                {lang.name}
+            </span>
+        );
+    });
+
     return (
         <>
             <Header />
-            <Status />
-            <main></main>
+            <main>
+                <Status />
+                <section className="languages flex fl-cntr">{languageChips}</section>
+            </main>
         </>
     );
 }
