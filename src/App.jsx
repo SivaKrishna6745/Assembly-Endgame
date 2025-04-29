c94c2b23fd3a7797217853a31a2589a885d1f355
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-04-29 12:24:10 +0530

    - Accessibility enhancements.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     const maxNoOfGuesses = 8;
 
     const wrongGuessCount = guessedLetters.filter((letter) => !currentWord.includes(letter)).length;
+    const lastGuessedLetter = guessedLetters.length > 0 ? guessedLetters[guessedLetters.length - 1] : null;
     const hasWon = currentWord.split('').every((letter) => guessedLetters.includes(letter));
     const hasLost = wrongGuessCount === maxNoOfGuesses;
     let isGameOver = hasWon || hasLost;
@@ -19,15 +20,9 @@ function App() {
             backgroundColor: lang.backgroundColor,
         };
         return (
-            <>
-                <span
-                    key={lang.name}
-                    className={`language-chip ${wrongGuessCount > index ? 'lost' : ''}`}
-                    style={styles}
-                >
-                    {lang.name}
-                </span>
-            </>
+            <span key={lang.name} className={`language-chip ${wrongGuessCount > index ? 'lost' : ''}`} style={styles}>
+                {lang.name}
+            </span>
         );
     });
 
@@ -113,10 +108,23 @@ function App() {
                 }
                 <section className="languages flex fl-cntr">{languageChips}</section>
                 <section className="current-word flex fl-cntr">{currentWordsSection}</section>
-                <section className="sr-only" aria-live="polite" role="status">
-                    current word is{' '}
-                    {currentWord.split('').map((letter) => (guessedLetters.includes(letter) ? letter + '.' : 'blank'))}
-                </section>
+                {guessedLetters.length > 0 && (
+                    <section className="sr-only" aria-live="polite" role="status">
+                        <p>
+                            {currentWord.includes(lastGuessedLetter)
+                                ? `Correct! The letter ${lastGuessedLetter} is in the word.`
+                                : `Wrong! The letter ${lastGuessedLetter} is not in the word.`}
+                            You have {maxNoOfGuesses - wrongGuessCount} attempts left.
+                        </p>
+                        <p>
+                            Current word is{' '}
+                            {currentWord
+                                .split('')
+                                .map((letter) => (guessedLetters.includes(letter) ? letter + '.' : 'blank'))
+                                .join('')}
+                        </p>
+                    </section>
+                )}
                 <section className="keyboard flex fl-cntr">{keyboard}</section>
                 {isGameOver && <button className="new-game btn">New Game</button>}
             </main>
